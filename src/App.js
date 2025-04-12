e1de34dae7055a17b80051fe45a740aa551fa0af
Laura Pociuniene <laurapociuniene@gmail.com>
2025-04-12 16:46:56 +0300

    use useReducerHook to manage state change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 import "./App.css";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  isLoading: false,
+  error: null,
+  data: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "getUsersStart": {
+      return {
+        ...state,
+        isLoading: true,
+      };
+    }
+    case "getUsersSuccess": {
+      return {
+        ...state,
+        isLoading: false,
+        data: action.payload,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
 
 function App() {
-  const [data, setData] = useState([]);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log("state", state);
 
   useEffect(() => {
+    dispatch({ type: "getUsersStart" });
+
     const getData = () => {
       axios.get("http://localhost:3000/users").then((response) => {
-        setData(response.data);
+        setTimeout(() => {
+          dispatch({ type: "getUsersSuccess", payload: response.data });
+        }, 1000);
       });
     };
 
@@ -16,8 +48,9 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      {data.map((user) => (
+    <div>
+      {state.isLoading && <div>Loading... </div>}
+      {state.data?.map((user) => (
         <div key={user.id}>{user.name}</div>
       ))}
     </div>
